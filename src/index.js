da68d96aa752787ea11c56cc98db0504bf9b05b1
Robyn H Richardson <robynhrich@icloud.com>
2023-12-18 23:22:10 -0500

    Added new fonts with basic HTML and CSS

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generateJoke(event) {
   event.preventDefault();
   let apiKey = "1o334f343ac7e4f31b2ae638t364b007";
   let context =
-    "Your are a funny AI Assistant that tells short and sweet jokes. The joke must be provided in HTML format. Example: <p>this is a joke</p>";
-  let prompt = "Generate a short and unique joke a French people. ";
+    "Your are a funny AI Assistant that tells jokes. The joke must be provided in HTML format. Example: <p>this is a joke</p>";
+  let prompt = "Generate a random joke about animals.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let jokeElement = document.querySelector("#joke");
 
-  jokeElement.innerHTML = "Generating a joke for you.. please wait";
+  jokeElement.innerHTML = "Dad joke loading...please wait";
 
   console.log("called the AI api");
   axios.get(apiUrl).then(displayJoke);
